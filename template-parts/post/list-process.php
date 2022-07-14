916ff757c604acd35f406d4ac7b32c5ffbda010e
Chia Yi Lai <cheesbe@gmail.com>
2022-07-14 15:58:00 +0800

    style: process

--- template-parts/post/list-process.php
+++ template-parts/post/list-process.php
@@ -24,23 +24,15 @@
                 $recent_posts = wp_get_recent_posts( $args );
                 foreach( $recent_posts as $key => $post) {
                     $link = get_permalink($post['ID']);
-                    $my_post_categories = get_the_category($post['ID']);
-                    $my_post_child_cats = array();
-                    foreach ( $my_post_categories as $post_cat ) {
-                        if ( 0 != $post_cat->category_parent ) {
-                            $my_post_child_cats[] = $post_cat->cat_name;
-                        }
-                    }
             ?>
                 <div class="<?=$postItemClass?> post-item">
                     <a href="<?=$link?>">
-                        <div class="post-img">
-                            <img src="<?=get_feature_image($post['ID'])?>" alt="<?=$post['post_title']?>">
+                        <div class="">
+                            <img src="<?=get_feature_image($post['ID'])?>" alt="<?=$post['post_title']?>" class="w-100">
                         </div>
                         <div class="post-text">
-                            <label class="post-sub-cat"><?=$my_post_child_cats[0]?></label>
                             <h3 class="post-title max-two-lines"><?=$post['post_title']?></h3>
-                            <p class="post-excerpt max-three-lines"><?=$post['post_excerpt']?></p>
+                            <p class="post-excerpt max-three-lines mb-0"><?=$post['post_excerpt']?></p>
                         </div>
                     </a>
                 </div>
