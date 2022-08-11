d73d3aac4ec6aeb0a608f5539730ebd387c5d234
ChiaYiLai <cheesebe@gmail.com>
2022-08-11 12:18:27 +0800

    test

--- template-parts/post/category.php
+++ template-parts/post/category.php
@@ -2,7 +2,6 @@
     $categories = get_the_category();
     $term = get_queried_object();
     $isProjects = $term->slug === 'projects';
-    var_dump('test');
 ?>
 
 <div class="col-12 col-sm-6 post-item">
@@ -12,6 +11,14 @@
                 <img src="<?=get_feature_image($id)?>" alt="<?=the_title()?>">
             </a>
         </div>
-        
+        <div class="post-text">
+            <h3 class="post-title max-two-lines">
+                <a href="<?=the_permalink();?>"><?=the_title()?></a>
+            </h3>
+            <?php if(!$isProjects): ?>
+            <p class="post-excerpt max-three-lines"><?=get_the_excerpt()?></p>
+            <?php endif; ?>
+        </div>
+       
     </div>
 </div>
