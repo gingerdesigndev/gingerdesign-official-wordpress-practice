8f56ddf01b3f082b8420f3bd21b86097dcd51754
ChiaYiLai <cheesebe@gmail.com>
2023-02-09 12:48:10 +0800

    fix: remove vardump

--- index.php
+++ index.php
@@ -19,7 +19,6 @@ $feedback = get_field('feedback', $homeDataPage->ID);
 $clientsImgDesktop = get_field('clients_img_desktop', $homeDataPage->ID);
 $clientsImgMobile = get_field('clients_img_mobile', $homeDataPage->ID);
 get_header();
-var_dump($feedback);
 ?>
 
 <div class="index-wrap">
