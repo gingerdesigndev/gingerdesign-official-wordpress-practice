1cf32c817019b01ec7dc52e9f7805a700e4f7dd1
ChiaYiLai <cheesebe@gmail.com>
2023-03-31 10:21:15 +0800

    test: home

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-網站更新中...
+<?php
+
+$homeDataPage = get_page_by_path('home-data');
+$feedback = get_field('feedback', $homeDataPage->ID);
+$clientsImgDesktop = get_field('clients_img_desktop', $homeDataPage->ID);
+$clientsImgMobile = get_field('clients_img_mobile', $homeDataPage->ID);
+get_header();
+?>
+
+<?php
+get_footer();
